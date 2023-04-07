27c956730aeb171448596deb0f2c64208f471120
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-04-08 04:39:23 +0600

    css work

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirts from '../Tshirts/Tshirts';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tShirts = useLoaderData();
+    const [cart, setCart] = useState([])
+
+    const handleAddToCart = tshirt =>{
+        console.log(tshirt);
+    }
     return (
-        <div>
-            <h3>This is my Home Compo.........!!!!!!!!! {tShirts.length}</h3>
+        <div className='home-container'>
+            <div className="t-shirts-container">
+            {
+               tShirts.map(tshirt => <Tshirts
+               key={tshirt._id}
+               tshirt={tshirt}
+               handleAddToCart={handleAddToCart}
+               ></Tshirts>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
---
+++ src/Component/Tshirts/Tshirts.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Tshirts.css'
+
+const Tshirts = ({tshirt, handleAddToCart}) => {
+    const {picture, name, price} = tshirt;
+
+    return (
+        <div className='tshirt-product'>
+            <img src={picture} alt="" />
+            <h4>{name}</h4>
+            <p>${price}</p>
+            <button onClick={handleAddToCart}>Buy Now</button>
+        </div>
+    );
+};
+
+export default Tshirts;
